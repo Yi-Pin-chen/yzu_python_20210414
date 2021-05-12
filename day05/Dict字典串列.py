86e6409825a8abd6bcd2cffd12c4e0caa44a31c6
Yi-Pin Chen <yipin1227@gmail.com>
2021-05-12 21:03:23 +0800

    python

--- day05/Dict字典串列.py
+++ day05/Dict字典串列.py
@@ -1,1 +1,17 @@
-salary ={''}
+#dict = key + value
+salary ={'John':50000,'Mary':68000}
+#salary ={'John'---Key :50000---value,'Mary':68000}
+print(salary)
+print("John 's salary is ", salary["John"])
+
+for key in salary:
+    print("%s 的薪資 %d "%(key,salary[key]))
+# 新增元素
+salary.setdefault("Bob",70000)
+print(salary)
+
+#求薪資總和
+TotalSalary = 0
+for key in salary:
+    TotalSalary = TotalSalary + salary[key]
+print("薪資總和:", TotalSalary )
